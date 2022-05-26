5d235d733f1a7e88f7699422c562153211d2516d
hangminhson <95537221+hangminhson@users.noreply.github.com>
2022-05-26 20:28:01 +0700

    Add files via upload

---
+++ HangMinhSon_61134291_cau2/app/src/main/java/hangminhson/example/hangminhson_61134291_cau2/ShowListTho.java
@@ -0,0 +1,14 @@
+package hangminhson.example.hangminhson_61134291_cau2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowListTho extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_list_tho);
+    }
+}
